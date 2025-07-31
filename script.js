988a73009210076c63001164e23c7fac53c7e65e
Ndima-karabo17 <mhangwanikarabo@gmail.com>
2025-07-31 08:52:48 +0200

    Update script.js
    I added action when you click a button from index.html it have to keep on changing depending on what you want to do. Here is See less or see more

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function SeeLess(){
-const details = document.getElementById("first-div");
-      const myButton = Event.target;
+function SeeLess(event) {
+  const details = document.getElementById("first-div");
+  const myButton = event.target;
 
-      if (details.style.display === "none") {
-        details.style.display = "block";
-        myButton.textContent = "Read Less...";
-      } else {
-        details.style.display = "none";
-        myButton.textContent = "Read More...";
-      }
-
+  if (details.style.display === "none" || details.style.display === "") {
+    details.style.display = "flex";
+    myButton.textContent = "SEE LESS...";
+  } else {
+    details.style.display = "none";
+    myButton.textContent = "SEE MORE...";
+  }
 }
